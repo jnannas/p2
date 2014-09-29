40c47980850881a0e3d612e6153f64393d1e9706
jnannas <jwnannas@gmail.com>
2014-09-29 17:39:22 -0400

    Initial HTML form setup, and basic css

--- index.php
+++ index.php
@@ -10,9 +10,24 @@
 		<?php require 'logic.php'; ?>
 </head>
 <body>
-		<h1>Password Generator</h1>
+		<h1>xkcd Password Generator</h1>
 
-		<?php echo $hello ?>
+		<h2 class="password">PASSWORD RESULT</h2>
+
+		<form>
+
+			<label for='numberWords'>How many words</label>
+			<input maxlength=1 type='text' name='numberWords' id='numberWords' value=' '>
+			<br/>
+			<input type='checkbox' name='includeNumber' id='includeNumber'>
+			<label for='includeNumber'>Include a Number</label>
+			<br/>
+			<input type='checkbox' name='includeSymbol' id='includeSymbol'>
+			<label for='includeSymbol'>Include a Symbol</label>
+			<br/>
+			<input type='submit' value='Generate Again'>
+
+		</form>
 		
 </body>
 </html>
